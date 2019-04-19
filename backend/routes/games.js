dec6e552a3fae9227e7ba383bdcc6e5f994c8bd9
Duvan G <duvan258@gmail.com>
2019-04-19 09:28:20 -0500

    BE: Clean code and improve init game response

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -3,45 +3,6 @@ var router = express.Router();
 const playerControler = require('../controllers/player.controller');
 const gameControler = require('../controllers/game.controller');
 
-/* POST game */
-/*router.post('/', function(req, res) {
-
-  console.log('*****************');
-  console.log(req.body);
-
-  /*let error = false;
-  let fieldWithError = '';
-  if(!req.body.firstName) {
-    error = true;
-    fieldWithError = 'First name';
-  } else if(!req.body.lastName) {
-    error = true;
-    fieldWithError = 'Last name';
-  } else if(!req.body.email) {
-    // TODO: Validate that the email format is valid
-    error = true;
-    fieldWithError = 'Email';
-  } else if(!req.body.subject) {
-    error = true;
-    fieldWithError = 'Subject';
-  }
-
-  if (error) {
-    return res.status(400).send({
-      success: 'false',
-      message: `${fieldWithError} is required`
-    });
-  }*/
-
-
-  /*return res.status(200).send({
-    success: 'true',
-    message: 'Game created successfully'
-  })
-});*/
-
-// router.post('/', player_controller.create_player);
-
 /* Init game */
 router.post('/', async function(req, res, next) {
 
@@ -66,6 +27,8 @@ router.post('/', async function(req, res, next) {
     let player1 = await playerControler.createPlayer(req.body.player1);
     let player2 = await playerControler.createPlayer(req.body.player2);
     game = await gameControler.createGame(player1, player2);
+    game.player1 = player1;
+    game.player2 = player2;
   } catch(err) {
     next(err);
   }
